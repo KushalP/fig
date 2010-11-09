11bf90b8a1134c09287f43bc9c37e6d236860a71
Kushal Pisavadia <kushi.p@gmail.com>
2010-11-09 10:41:19 +0000

    got first test to pass 'loosely' when only a single string without wildcards is provided and exists

--- go/wildcard.go
+++ go/wildcard.go
@@ -1,5 +1,12 @@
 package fig
 
+import "strings"
+
 func findFiles(fs FileSystem, baseDir string, pattern string) []string {
+	if !strings.Contains(pattern, "*") {
+		if fs.Exists(pattern) {
+			return []string{strings.Trim(pattern, "")}
+		}
+	}
 	return []string{}
 }
