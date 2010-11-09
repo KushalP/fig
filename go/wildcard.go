6c5faa6a20c594bf422ce5ebdd51cf07aface9c3
Kushal Pisavadia <kushi.p@gmail.com>
2010-11-09 10:46:14 +0000

    Adding in comment to help when this code needs to be looked at for a future date

--- go/wildcard.go
+++ go/wildcard.go
@@ -3,7 +3,8 @@ package fig
 import "strings"
 
 func findFiles(fs FileSystem, baseDir string, pattern string) []string {
-	if !strings.Contains(pattern, "*") {
+	// When there's no wildcard specified and only a single string provided
+    if !strings.Contains(pattern, "*") {
 		if fs.Exists(pattern) {
 			return []string{strings.Trim(pattern, "")}
 		}
